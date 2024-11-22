aa99299a88a6dc26000c59ca883288675fc8f735
kraibse <33869366+kraibse@users.noreply.github.com>
2024-11-22 07:37:45 +0000

    Added a basic player with ability to move

---
+++ player.py
@@ -0,0 +1,39 @@
+import pygame
+import math
+
+from animated_sprite import SpriteAnimation
+
+class Player():
+    max_speed = 0
+
+    def __init__(self, level):
+        self.sprite = SpriteAnimation('assets/player/idle/', 'player0', 1)
+        self.position = [math.floor(level.w / 2, 0), math.floor(level.h / 2, 0)]
+
+    def draw(self):
+        self.sprite.draw()
+
+    def is_accelerating(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            return True
+        
+        return False
+
+    def move(self):
+        mousex, mousey = pygame.mouse.get_pos()
+        mx = mousex - self.position[0]
+        my = mousey - self.position[1]
+
+        distance = math.sqrt(mx ** 2 + my ** 2)
+
+        # TODO: accelerate and deccelerate smoothly
+        motion = [mx / distance, my / distance] * self.max_speed    # move the player at a constant speed for now
+
+        if self.is_accelerating():
+            self.position[0] + motion[0]
+            self.position[1] + motion[1]
+
+
+    def mine(self):
+        pass

c144812e7afd3bebc84ab613adea985c483514f7
kraibse <33869366+kraibse@users.noreply.github.com>
2024-11-22 07:43:20 +0000

    Updated the level with the draw function

--- levels.py
+++ levels.py
@@ -2,9 +2,29 @@ class Scene:
     def __init__(self,screen,scene_manager) -> None:
         self.screen = screen
         self.scene_manager = scene_manager
+        self.player = self.scene_manager.player
+
+        self.enemies = self.spawn_enemies()
+        self.planet = self.spawn_planets()
     
-    def run():
+    def run(self):
         pass
+
+    def draw(self):
+        self.player.draw()
+
+        for enemy in self.enemies:
+            enemy.draw()
+
+        for planet in self.planets:
+            planet.draw()
+
+    def spawn_enemies(self):
+        return []
+    
+    def spawn_planets(self):
+        return []
+
 
 class Level1(Scene):
     def run(self):

df1c2a22e7318c185c265448a6c9cfd19cd1a24f
kraibse <33869366+kraibse@users.noreply.github.com>
2024-11-26 10:06:53 +0100

    Added a single frame draw method for the player + docstring

--- player.py
+++ player.py
@@ -1,26 +1,56 @@
+'''
+@author: Leon Horn
+@version: 1.0
+
+@datum: 2024-11-26
+
+@description: Spielerklasse mit animierten Sprites und der Bewegungsmoeglichkeit
+'''
+
+import os
+import asyncio
 import pygame
 import math
 
 from animated_sprite import SpriteAnimation
+from globals import SCREEN_W, SCREEN_H
 
 class Player():
     max_speed = 0
 
     def __init__(self, level):
-        self.position = [math.floor(level.w / 2), math.floor(level.h / 2)]
+        w = SCREEN_W
+        h = SCREEN_H
+        
+        self.level = level
+        
+        self.position = [math.floor(w / 2), math.floor(h / 2)]
+        base_path = os.path.dirname(os.path.abspath(__file__)) + '/assets/sprites/player/'
+        print(base_path)
+        
         self.animations = {
-            "idle": SpriteAnimation('/home/kraibse/projects/scavanger/assets/sprites/player/', 'player0', 1)
+            "idle": SpriteAnimation(base_path, 'player0', 1)
         }
 
         self.current_animation = "idle"
-        self.get_current_animation().play()
+        # asyncio.run(self.get_current_animation().play())
 
     def change_animation(self, new_anim="idle"):
         animation = self.get_current_animation()
-        animation.stop()
+        asyncio.run(animation.stop())
         
         self.current_animation = new_anim
-        animation.play()
+        asyncio.run(animation.play())
+        
+    def draw(self):
+        current_sprite = self.get_current_animation()._get_current_sprite()
+        print(current_sprite)
+        
+        if current_sprite is None:
+            return
+        
+        print("Drawing player at " + str(self.position[0]) + ", " + str(self.position[1]))
+        self.level.screen.blit(current_sprite, (self.position[0], self.position[1]))
 
     def get_current_animation(self):
         return self.animations.get(self.current_animation)
@@ -40,6 +70,8 @@ class Player():
         my = mousey - self.position[1]
 
         distance = math.sqrt(mx ** 2 + my ** 2)
+        if distance == 0:
+            return
 
         # TODO: accelerate and deccelerate smoothly
         motion = [mx / distance, my / distance] * self.max_speed    # move the player at a constant speed for now

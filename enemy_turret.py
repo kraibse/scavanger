1dfdd68e219223ba363801262d2091cb87463b8d
kraibse <33869366+kraibse@users.noreply.github.com>
2024-11-29 11:45:16 +0000

    Added bullets

--- enemy_turret.py
+++ enemy_turret.py
@@ -9,12 +9,22 @@
 import pygame
 import math
 import globals
+
+from projectile import Projectile
 from animated_sprite import SpriteAnimation
 from scavengeable import Scavengeable
 
 class EnemyTurret(Scavengeable):
     def __init__(self, screen, maxDurability, resourceDrops):
         super().__init__(screen, maxDurability, resourceDrops)
+
+        self.projectiles = [
+            Projectile(self),
+            Projectile(self),
+            Projectile(self),
+            Projectile(self),
+            Projectile(self)
+        ]
 
         self.position = [0, 0]
         self.animation = SpriteAnimation(globals.BASE_PATH + 'assets/sprites/enemy/turret/', 'turret0', 1, '.png')
@@ -28,8 +38,20 @@ class EnemyTurret(Scavengeable):
         print(self.sprite)
         
         self.screen.blit(self.sprite, self.rect)
+
+        for bullet in self.projectiles:
+            if bullet.is_active and bullet.origin.distance_to(bullet.position) > globals.BULLET_DECAY_DISTANCE:
+                bullet.reset()
+            elif bullet.is_active and bullet.position.distance_to(pygame.Vector2(self.player.position[0], self.player.position[1])):
+                globals.current_player_health -= 1
+                bullet.reset()
+            else:
+                bullet.move()
+                bullet.draw(self.screen)
+
     
     def defend(self, player):
+        self.player = player
         sx = self.position[0] - globals.camera_offset_x
         sy = self.position[1] - globals.camera_offset_y
         self_position = pygame.Vector2(sx, sy)
@@ -44,3 +66,7 @@ class EnemyTurret(Scavengeable):
         if distance < globals.ENEMY_TURRET_RANGE:
             angle = math.degrees(math.atan2(-ry, rx)) - 90
             self.sprite = pygame.transform.rotate(self.animation._get_current_sprite(), angle)
+            
+            for b in self.projectiles:
+                if b.is_active == False:
+                    b.shoot_at(rx, ry)
--- projectile.py
+++ projectile.py
@@ -10,15 +10,30 @@
 import pygame
 
 class Projectile:
-    def __init__(self, x, y, angle):
+    def __init__(self, entity, x, y, angle):
+        self.entity = entity
         self.x = x
         self.y = y
         self.angle = angle
         self.speed = 10
+        self.is_active = False
+
+        self.origin = pygame.Vector2(entity.position[0], entity.position[1])
+        self.position = pygame.Vector2(entity.position[0], entity.position[1])
 
     def shoot_at(self, target_position):
+        self.is_active = True
         self.direction = target_position - pygame.Vector2(self.x, self.y)
     
+    def draw(self, screen):
+        start_pos = self.position - self.direction.scale_to_length(4)
+        end_pos = (self.position.x, self.position.y)
+        pygame.draw.line(screen, pygame.Color(255, 0, 0), (start_pos.x, start_pos.y), (self.position.x, self.position.y))
+
     def move(self):
         self.direction = self.direction.scale_to_length(self.speed)
         self.position = pygame.Vector2(self.x + self.direction.x, self.y + self.direction.y)
+
+    def reset(self):
+        self.position = self.origin
+        self.is_active = False
